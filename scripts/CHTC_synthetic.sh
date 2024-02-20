c13219d59033009f16b5361d31836c1fa3674e09
Huanran Li <nemo0421@gmail.com>
2024-02-20 11:28:29 -0600

    update

--- scripts/CHTC_synthetic.sh
+++ scripts/CHTC_synthetic.sh
@@ -3,11 +3,11 @@
 cd ../src/
 
 # Calculate the expression
-mr=$(expr $1 + 0)
+mr=$(expr $1 + 1)
 
 # Display the result
-echo "0.$mr"
+# echo "0.$mr"
 
-python main.py --missing_rate 0.$mr --step_method Regular --max_iter 2000 --step_size 1 --num_rums 3 --experiment_name Synthetic_MR0.$mr
+python main.py --missing_rate 0.$mr --step_method Regular --max_iter 10000 --step_size 1 --num_rums 1 --experiment_name CHTC_ITER10K_Synthetic_MR0.$mr
 
 cd ../scripts
