adad62ebdb5eec7e425bb06ceb045f4b20cb56c5
Huanran Li <nemo0421@gmail.com>
2024-02-21 12:03:20 -0600

    Update CHTC_synthetic_largeLambda.sh

--- scripts/CHTC_synthetic_largeLambda.sh
+++ scripts/CHTC_synthetic_largeLambda.sh
@@ -8,6 +8,6 @@ mr=$(expr $1 + 1)
 # Display the result
 # echo "0.$mr"
 
-python main.py --missing_rate 0.$mr --step_method Regular --max_iter 10000 --step_size 1 --num_rums 1 -lambda_in 0.001 --experiment_name CHTC_ITER10K_Synthetic_Lambda1e-3_MR0.$mr
+python main.py --missing_rate 0.$mr --step_method Regular --max_iter 10000 --step_size 1 --num_rums 1 --lambda_in 0.001 --experiment_name CHTC_ITER10K_Synthetic_Lambda1e-3_MR0.$mr
 
 cd ../scripts
