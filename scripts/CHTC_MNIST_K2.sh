f3ce08e3db69bc750d0aee95d1c002c56ad40b53
Huanran Li <nemo0421@gmail.com>
2024-02-27 15:39:46 -0600

    Update CHTC_MNIST_K2.sh

--- scripts/CHTC_MNIST_K2.sh
+++ scripts/CHTC_MNIST_K2.sh
@@ -5,6 +5,8 @@ cd ../src/
 # Calculate the expression
 mr=$(expr $1 + 1)
 
+echo "MR:0.$mr"
+
 python main.py --missing_rate 0.$mr --step_method Regular --max_iter 100 --num_runs 3 --step_size 1 --num_cluster 2 --samples_per_class 50 --dataset MNIST --experiment_name CHTC_MNIST
 
 cd ../scripts
