f5e625f4558285036ac75dcf8c0d7ec43acbbfaf
Huanran Li <nemo0421@gmail.com>
2024-03-01 22:44:57 -0600

    update

--- scripts/CHTC_MNIST_K2.sh
+++ scripts/CHTC_MNIST_K2.sh
@@ -3,10 +3,11 @@
 cd ../src/
 
 # Calculate the expression
-mr=$(expr $1 + 1)
+# mr=$(expr $1 + 1)
+mr=$(expr \( $1 % 9 \) + 1 )
 
 echo "MR:0.$mr"
 
-python main.py --missing_rate 0.$mr --step_method Regular --max_iter 200 --num_runs 3 --step_size 1 --num_cluster 2 --samples_per_class 50 --dataset MNIST --experiment_name CHTC_MNIST
+python main.py --missing_rate 0.$mr --step_method Regular --max_iter 1000 --num_runs 1 --step_size 1 --num_cluster 2 --samples_per_class 50 --dataset MNIST --experiment_name CHTC_MNIST_0.$mr
 
 cd ../scripts
--- scripts/CHTC_sims.sh
+++ scripts/CHTC_sims.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the number of times you want to run the script
-number_of_runs=9
-script="LOCAL_synthetic_K2.sh"
+number_of_runs=36
+script="CHTC_MNIST_K2.sh"
 
 # Loop and call other_script.sh with index
 for (( i=0; i<number_of_runs; i++ ))
