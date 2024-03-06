477f20a6a02675ddae08072b15d3cf495b8f9206
Huanran Li <nemo0421@gmail.com>
2024-03-06 11:29:59 -0600

    Update CHTC_Hopkins.sh

--- scripts/CHTC_Hopkins.sh
+++ scripts/CHTC_Hopkins.sh
@@ -4,12 +4,12 @@ cd ../src/
 
 # Calculate the expression
 # mr=$(expr $1 + 1)
-mr=$(expr \( $1 % 9 \) + 1 )
-idx=$(expr $1 / 9)
+mr=$(expr \( $1 % 6 \) )
+idx=$(expr $1 / 6)
 
 echo "MR:0.$mr"
 echo "Hopkins_index:$idx"
 
-python main.py --Hopkins_index $idx --missing_rate 0.$mr --step_method Regular --max_iter 3000 --step_size 1 --dataset Hopkins155 --experiment_name CHTC_Hopkins155_ITER3k_MR0.$mr
+python main.py --Hopkins_index $idx --missing_rate 0.$mr --step_method Regular --max_iter 2000 --step_size 1 --dataset Hopkins155 --experiment_name CHTC_Hopkins155_ITER2k_MR0.$mr
 
 cd ../scripts
