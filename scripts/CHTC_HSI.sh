f6b4784a76f03ea55fea921c7605b24ba7d24b87
Huanran Li <nemo0421@gmail.com>
2024-03-13 11:46:50 -0500

    HSI Lambdas

--- scripts/CHTC_HSI.sh
+++ scripts/CHTC_HSI.sh
@@ -6,11 +6,13 @@ cd ../src/
 # mr=$(expr $1 + 1)
 mr=$(expr \( $1 % 9 \) + 1 )
 idx=$(expr \( $1 / 9 \) % 5 )
+lambda=1e-5
 
 echo "MR:0.$mr"
 echo "HSI_idx:$idx"
+echo "lambda:$lambda"
 echo
 
-python main.py --missing_rate 0.$mr --step_method Regular --max_iter 6000 --step_size 1 --dataset HSI_$idx --experiment_name CHTC_HSI_6K_MR0.$mr --check_acc_per_iter 100 --num_cluster 2 --samples_per_class 50
+python main.py --lambda_in $lambda --missing_rate 0.$mr --step_method Regular --max_iter 6000 --step_size 1 --dataset HSI_$idx --experiment_name CHTC_HSI_6K_MR0.$mr --check_acc_per_iter 100 --num_cluster 2 --samples_per_class 50
 
 cd ../scripts
