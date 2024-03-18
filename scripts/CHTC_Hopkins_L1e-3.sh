c8048cedba56841dcc2e0c0a396ab0e877ae10da
Huanran Li <nemo0421@gmail.com>
2024-03-18 13:24:14 -0500

    Update CHTC_Hopkins_L1e-3.sh

--- scripts/CHTC_Hopkins_L1e-3.sh
+++ scripts/CHTC_Hopkins_L1e-3.sh
@@ -12,6 +12,6 @@ echo "MR:0.$mr"
 echo "Hopkins_index:$idx"
 echo "lambda value: $lambda"
 
-echo python main.py --missing_rate 0.$mr --lambda_in $lambda --step_method Regular --max_iter 12001 --step_size 1 --dataset Hopkins155_$idx --experiment_name Hopkins155_Lambda{$lambda}_ITER12k_MR0.$mr --check_acc_per_iter 500
+python main.py --missing_rate 0.$mr --lambda_in $lambda --step_method Regular --max_iter 12001 --step_size 1 --dataset Hopkins155_$idx --experiment_name Hopkins155_Lambda{$lambda}_ITER12k_MR0.$mr --check_acc_per_iter 500
 
 cd ../scripts
